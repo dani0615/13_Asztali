d6eafae59217b27f7690ae59ceeb74b844d952a2
haluskad <haluskad@WINDOWS-V9J9J6T>
2025-11-25 11:02:49 +0100

    További tesztek , betű számolás , páros stb

--- Teszteles_11_25/Teszteles_11_25/Matematika.cs
+++ Teszteles_11_25/Teszteles_11_25/Matematika.cs
@@ -13,5 +13,51 @@ namespace Teszteles_11_25
             return a * a;
         }
 
+        public double Osztas(double a, double b) 
+        {
+            return a / b;
+        }
+
+
+        public int Abetuszama(string szoveg) 
+        {
+            int db = 0;
+            
+            if (szoveg == null) 
+            {
+                db = 0;
+               return db;
+            }
+
+            foreach (char c in szoveg) 
+            {
+                if (c == 'A' || c == 'a') 
+                {
+                    db++;
+                }
+            }
+            return db;
+
+           
+
+        }
+
+        public bool isParos(int a) 
+        {
+            if (a % 2 == 0) 
+            {
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+
+
+
+
+
+
     }
 }
--- Teszteles_11_25/Teszteles_11_25Tests/MatematikaTests.cs
+++ Teszteles_11_25/Teszteles_11_25Tests/MatematikaTests.cs
@@ -5,16 +5,106 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using Microsoft.VisualStudio.TestPlatform.Common.Interfaces;
 
 namespace Teszteles_11_25.Tests
 {
     [TestClass()]
+
     public class MatematikaTests
     {
-        [TestMethod()]
-        public void NegyzetTest()
+        private Matematika m;
+        [TestInitialize]
+        public void Setup()
         {
-            Assert.Fail();
+            m = new Matematika();
         }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            m = null;
+        }
+
+
+
+        [TestMethod()]
+        [DataRow(5)]
+        [DataRow(0)]
+        [DataRow(-4)]
+        [DataRow(int.MaxValue)]
+
+        public void NegyzetTest(int adat)
+        {
+
+            // Act
+            int actualEredmeny = m.Negyzet(adat);
+
+            // Assert 
+            long varhatoEredmeny = (long)adat * adat;
+
+            Assert.AreEqual(varhatoEredmeny, actualEredmeny);
+        }
+
+        [TestMethod()]
+        [DataRow(5, 2)]
+
+        public void OsztasTest(int a, int b)
+        {
+            double expected = (double)a / b;
+
+            double act = m.Osztas(a, b);
+            Assert.AreEqual(expected, act);
+
+        }
+
+        [TestMethod()]
+        [DataRow(5, 0)]
+        public void OsztasTest_DivideByZero(int a, int b)
+        {
+            Assert.Throws<DivideByZeroException>(() => m.Osztas(a, b));
+        }
+
+        [TestMethod()]
+        [DataRow("Alma", 2)]        
+        [DataRow("Ananász", 2)]
+        [DataRow("Körte", 0)]
+        [DataRow("",0)]
+        public void AbetuszamaTest(string szoveg, int elvartDb)
+        {
+           
+            int actualDb = m.Abetuszama(szoveg);
+
+            
+            Assert.AreEqual(elvartDb, actualDb);
+
+        }
+
+
+        [TestMethod()]
+        [DataRow(null)]
+        public void AbetuNUllTest(string a) { 
+         
+           Assert.Throws<NullReferenceException>(() => m.Abetuszama(a));
+        }
+
+        [TestMethod()]
+        [DataRow(4, true)]
+        public void ParosETrueTest(int a, bool elvart)
+        {
+            bool actual = m.isParos(a);
+            Assert.AreEqual(elvart, actual);
+        }
+
+        [TestMethod()]
+        [DataRow(5, false)]
+        public void ParosEFalseTest(int a, bool elvart)
+        {
+            bool actual = m.isParos(a);
+            Assert.AreEqual(elvart, actual);
+        }
+
+
     }
 }
