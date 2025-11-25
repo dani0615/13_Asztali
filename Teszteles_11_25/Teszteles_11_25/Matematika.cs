b65dade0167855ca7dbe17b6b8ea0bca0ef0eb98
haluskad <haluskad@WINDOWS-V9J9J6T>
2025-11-25 09:24:16 +0100

    Teszteles feladat

---
+++ Teszteles_11_25/Teszteles_11_25/Matematika.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teszteles_11_25
+{
+    public class Matematika
+    {
+        public int Negyzet(int a) 
+        {
+            return a * a;
+        }
+
+    }
+}
---
+++ Teszteles_11_25/Teszteles_11_25Tests/MatematikaTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Teszteles_11_25;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teszteles_11_25.Tests
+{
+    [TestClass()]
+    public class MatematikaTests
+    {
+        [TestMethod()]
+        public void NegyzetTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
