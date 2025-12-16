3517c0f493fe38d952f456927f0bb7d0063b4079
haluskad <haluskad@WINDOWS-V9J9J6T>
2025-12-16 10:25:24 +0100

    Login form , maui

---
+++ LoginForm/LoginForm/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿namespace LoginForm
+{
+    public partial class MainPage : ContentPage
+    {
+        static List<Users> user = new List<Users>()
+        {
+            
+               new Users { Nev ="tibor" , Jelszo="erosjelszo123" },
+               new Users { Nev ="sandor" , Jelszo="sanyika01" },
+               new Users { Nev ="lacigaming" , Jelszo="fortnite69" }
+        };
+
+        public MainPage()
+        {
+            InitializeComponent(); 
+        }
+
+
+
+        private void btnBelépés_Clicked(object sender, EventArgs e)
+        {
+            string beirtNev = entNev.Text.Trim();
+            string beirtJelszo = entPassword.Text.Trim();
+
+            foreach (Users felhasznalo in user)
+            {
+                if (felhasznalo.Nev == beirtNev)
+                {
+                    if (felhasznalo.Jelszo == beirtJelszo)
+                    {
+                        DisplayAlert("Sikeres", $"Üdvözöllek, {felhasznalo.Nev}!", "OK");
+                        
+                    }
+                    else
+                    {
+                        DisplayAlert("Hiba", "Hibás jelszó!", "OK");
+                    }
+                    return; 
+                }
+            }
+
+            DisplayAlert("Hiba", "Nem létezik ilyen felhasználónév!", "OK");
+        }
+    }
+}
---
+++ LoginForm/LoginForm/Users.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginForm
+{
+    public class Users
+
+    {
+        public string Nev { get; set; }
+        public string Jelszo { get; set; }
+    }
+}
